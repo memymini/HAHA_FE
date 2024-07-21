05f522000448a12da84c90748e7dac368a21fdaa
haeun1107 <haeun9394@gmail.com>
2024-07-22 06:07:18 +0900

    [FEAT] 프로필 수정 구현

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Nav from "./components/nav/nav.js";
 import Login from "./pages/login/login.js";
 import Signup from "./pages/signup/signup.js";
 import Description from "./pages/description/description.js";
+import ProfileUpdate from "./pages/profileUpdate/profileUpdate.js";
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/description" element={<Description />} />
+          <Route path="/profileUpdate" element={<ProfileUpdate />} />
         </Routes>
       </div>
     </Router>
