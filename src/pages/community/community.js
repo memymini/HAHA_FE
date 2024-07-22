bbdc0e452032d5cc61983fbc2bfc14ab5d490507
memymini <minhi@gmail.com>
2024-07-22 10:15:39 +0900

    [fix]calendar날짜변경안된다아

--- src/pages/community/community.js
+++ src/pages/community/community.js
@@ -1,8 +1,13 @@
 import React from "react";
 import styles from "./community.module.css";
+import CommunityImg from "../../assets/images/community.png";
 
 function Community() {
-  return <div className="container">community</div>;
+  return (
+    <div className="container">
+      <img className="container" src={CommunityImg} alt="community"></img>
+    </div>
+  );
 }
 
 export default Community;
